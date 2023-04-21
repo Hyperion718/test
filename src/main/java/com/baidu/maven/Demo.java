917ccb639dbb1a76175c5af44fc9c8c8c6708caa
CQC <1819527994@qq.com>
2023-04-21 14:52:29 +0800

    Initial commit

---
+++ src/main/java/com/baidu/maven/Demo.java
@@ -0,0 +1,16 @@
+package com.baidu.maven;
+
+/**
+ * ClassName: Demo
+ * Package: com.baidu.maven
+ * Description:
+ *
+ * @Author Chen Qichao
+ * @Create 2023/4/19 10:37
+ * @Version 1.0
+ */
+public class Demo {
+    public void method01(){
+        System.out.println("我是maven");
+    }
+}
---
+++ src/test/java/com/baidu/test/DemoTest.java
@@ -0,0 +1,21 @@
+package com.baidu.test;
+
+import com.baidu.maven.Demo;
+import org.junit.Test;
+
+/**
+ * ClassName: DemoTest
+ * Package: com.baidu.test
+ * Description:
+ *
+ * @Author Chen Qichao
+ * @Create 2023/4/19 10:37
+ * @Version 1.0
+ */
+public class DemoTest {
+    @Test
+    public void method01(){
+        Demo demo = new Demo();
+        demo.method01();
+    }
+}
